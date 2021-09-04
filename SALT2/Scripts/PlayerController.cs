fb8a8358c18e7c9d34787339cccbc68bde46ce38
Patrick Dooley <patrickthe8@live.com>
2021-09-03 23:00:59 -0400

    Updated movement in player controller, improved test scene, added more placeholder animations.

--- SALT2/Scripts/PlayerController.cs
+++ SALT2/Scripts/PlayerController.cs
@@ -4,12 +4,15 @@ using System;
 public class PlayerController : KinematicBody
 {
     // Variables
-    [Export] private float MOVE_SPEED = 5;
-    [Export] private float JUMP_FORCE = 10;
     [Export] private float GRAVITY = 9.8f;
-    [Export] private float MAX_FALL_SPEED = 100;
+    [Export] private float MAX_SPEED = 10;
+    [Export] private float JUMP_HEIGHT = 100;
+    [Export] private float ACCELERATION = 5;
+    [Export] private float FRICTION = 0.2f;
+    [Export] private float AIR_FRICTION = 0.05f;
 
-    private float y_velocity = 0;
+    //private float y_velocity = 0;
+    private Vector3 motion;
     private bool facing_right = false;
 
     private AnimationPlayer anim_player;
@@ -33,57 +36,81 @@ public class PlayerController : KinematicBody
     public override void _PhysicsProcess(float delta)
     {
         base._PhysicsProcess(delta);
+
+        motion.y -= GRAVITY * delta;
+        bool friction = false;
         int move_dir = 0;
 
+        // Check for player directional inputs to add or subtract acceleration.
         if(Input.IsActionPressed("move_right"))
         {
+            motion.x = Math.Min((motion.x + ACCELERATION), MAX_SPEED);
             move_dir += 1;
+            // TODO: Add walk animation.
         }
-        if (Input.IsActionPressed("move_left"))
+        else if (Input.IsActionPressed("move_left"))
         {
+            motion.x = Math.Max((motion.x - ACCELERATION), -MAX_SPEED);
             move_dir -= 1;
+            // TODO: Add walk animation.
+        }
+        else
+        {
+            friction = true;
+            // TODO: Add idle animation.
         }
 
-        MoveAndSlide(new Vector3(move_dir * MOVE_SPEED, y_velocity, 0), Vector3.Up);
-
-        bool just_jumped = false;
-        bool grounded = IsOnFloor();
-        y_velocity -= GRAVITY * delta;
-
-        if(y_velocity < -MAX_FALL_SPEED)
+        // Jump logic. Checks if player is on floor before deciding if it is ok to jump.
+        // If player is in the air, play jump animation. Additionally, less friction is
+        // applied to the player while in the air.
+        if (IsOnFloor())
         {
-            y_velocity = -MAX_FALL_SPEED;
-        }
-
-        if(grounded)
-        {
-            y_velocity = -0.1f;
-            if(Input.IsActionPressed("move_jump"))
+            if (Input.IsActionPressed("move_jump"))
             {
-                y_velocity = JUMP_FORCE;
-                just_jumped = true;
+                motion.y = JUMP_HEIGHT;
+            }
+            if (friction == true)
+            {
+                motion.x = Mathf.Lerp(motion.x, 0, FRICTION);
+            }
+        }
+        else
+        {
+            // Play jump animation if going up.
+            if (motion.y > 0)
+            {
+                play_anim("test_jump_anim");
+            }
+            // Play falling animation if going down.
+            else
+            {
+                play_anim("test_fall_anim");
+            }
+            
+            // Air friction is applied while in the air.
+            if (friction == true)
+            {
+                motion.x = Mathf.Lerp(motion.x, 0, AIR_FRICTION);
             }
         }
 
+        // Decides when to call flip method.
         if(move_dir < 0 && facing_right)
         {
             flip();
         }
-        if(move_dir > 0 && !facing_right)
+        if (move_dir > 0 && !facing_right)
         {
             flip();
         }
 
-        if(just_jumped)
-        {
-            play_anim("test_jump_anim");
-        }
+        motion = MoveAndSlide(motion, Vector3.Up);
     }
 
     // Flips player model depending on direction faced.
     private void flip()
     {
-        Console.WriteLine("FLIP!");
+        //Console.WriteLine("FLIP!");
         graphics.RotateY(-1f);
         facing_right = !facing_right;
     }
