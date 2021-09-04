3f4d8a9873350ccfe3861cbc23a8cab8bc9b4183
Patrick Dooley <patrickthe8@live.com>
2021-09-04 19:50:40 -0400

    31 implement player shooting (#51)
    * Began adding bullet physics.
    
    * Added ability to fire weapon, changed collision masking, and edited test scene.
    
    * Updated gitignore to ignore test scene and results file.

---
+++ SALT2/Scripts/Controllers/BulletController.cs
@@ -0,0 +1,64 @@
+using System;
+using Godot;
+
+/// <summary>
+/// Script used to control all types of bullets.
+/// </summary>
+public class BulletController : RigidBody
+{
+    // Variables
+    [Export]
+    private int damage = 1;
+    [Export]
+    private int speed = 15;
+
+    private bool shoot = false;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether bullet is shot.
+    /// </summary>
+    public bool Shoot { get => shoot; set => shoot = value; }
+
+    /// <inheritdoc/>
+    public override void _Ready()
+    {
+        SetAsToplevel(true);
+    }
+
+    /// <inheritdoc/>
+    public override void _PhysicsProcess(float delta)
+    {
+        base._PhysicsProcess(delta);
+
+        if (shoot)
+        {
+            LinearVelocity = new Vector3(Transform.basis.z.Normalized() * speed);
+        }
+    }
+
+    /// <summary>
+    /// Hit detection code.
+    /// </summary>
+    /// <param name="body">Value for body that was hit by bullet.</param>
+    public void _on_Area_body_entered(PhysicsBody body)
+    {
+        if (body.IsInGroup("Enemy"))
+        {
+            GD.Print("Enemy Hit!");
+
+            // TODO: Add logic to do damage.
+            QueueFree();
+        }
+        else if (body.IsInGroup("Player"))
+        {
+            GD.Print("Player Hit!");
+
+            // TODO: Add logic to do damage.
+            QueueFree();
+        }
+        else
+        {
+            QueueFree();
+        }
+    }
+}
