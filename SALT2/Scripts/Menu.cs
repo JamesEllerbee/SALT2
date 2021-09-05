9cdbdc2b88195ecf6e0b1e673876d945c9eadc96
unknown <dmurtagh14@gmail.com>
2021-09-05 06:11:53 -0400

    added functionality to options for volume control and toggle full screen

--- SALT2/Scripts/Menu.cs
+++ SALT2/Scripts/Menu.cs
@@ -7,21 +7,19 @@ public class Menu : Control
 	{
 		GetNode<Button>("VBoxContainer/StartButton").GrabFocus();
 	}
+
+	public override void _Process(float delta)
+	{
+		//Makes music loop
+		if(!GetNode<AudioStreamPlayer2D>("MainMenuMusic").Playing){
+			GetNode<AudioStreamPlayer2D>("MainMenuMusic").Play();
+		}
+	}
 	
-	// public override void _Input(InputEvent inputEvent)
-	// {
-	// 	if (inputEvent.IsActionPressed("move_down") && )
-	// 		{
-	// 			GetNode<Button>("VBoxContainer/OptionsButton").FocusMode;
-	// 			GetFocusMode();
-				
-	// 		}
-	// }
-
-
 
 	private void _on_StartButton_pressed()
 	{
+	GetNode<AudioStreamPlayer>("StartSoundFX").Play();
 	GetTree().ChangeScene("res://Scenes/dev_test1.tscn");
 	}
 	
@@ -30,19 +28,64 @@ public class Menu : Control
 	GetTree().Quit();
 	}
 	
-	private void _on_StartButton_mouse_entered()
+	private void _on_OptionsButton_pressed()
 	{
-   		GetNode<Button>("VBoxContainer/StartButton").GrabFocus();
+		GetNode<VBoxContainer>("VBoxContainer").Visible = false;
+		GetNode<HSlider>("VolumeSlider").Visible = true;
+		GetNode<CheckButton>("FullScreenToggle").Visible = true;
+		GetNode<Button>("MainMenuButton").Visible = true;
+	
 	}
 	
-	private void _on_OptionsButton_mouse_entered()
+	
+	private void _on_VolumeSlider_value_changed(float value)
 	{
-		GetNode<Button>("VBoxContainer/OptionsButton").GrabFocus();
+		AudioServer.SetBusVolumeDb(AudioServer.GetBusIndex("Master"), value);
 	}
 	
-	private void _on_QuitButton_mouse_entered()
+	private void _on_FullScreenToggle_pressed()
 	{
-		GetNode<Button>("VBoxContainer/QuitButton").GrabFocus();
+		OS.WindowFullscreen = !OS.WindowFullscreen; //toggles full screeen off/on
+	}
+	
+	private void _on_MainMenuButton_pressed()
+	{
+		GetNode<VBoxContainer>("VBoxContainer").Visible = true;
+		GetNode<HSlider>("VolumeSlider").Visible = false;
+		GetNode<CheckButton>("FullScreenToggle").Visible = false;
+		GetNode<Button>("MainMenuButton").Visible = false;
 	}
 
+
+private void _on_StartButton_mouse_entered()
+{
+	GetNode<Button>("VBoxContainer/StartButton").GrabFocus();
 }
+
+private void _on_OptionsButton_mouse_entered()
+{
+	GetNode<Button>("VBoxContainer/OptionsButton").GrabFocus();
+}
+
+private void _on_QuitButton_mouse_entered()
+{
+	GetNode<Button>("VBoxContainer/QuitButton").GrabFocus();
+}
+
+private void _on_StartButton_focus_entered()
+{
+	GetNode<AudioStreamPlayer>("SelectSoundFX").Play();
+}
+
+private void _on_OptionsButton_focus_entered()
+{
+	GetNode<AudioStreamPlayer>("SelectSoundFX").Play();
+}
+
+private void _on_QuitButton_focus_entered()
+{
+	GetNode<AudioStreamPlayer>("SelectSoundFX").Play();
+}
+
+}
+
