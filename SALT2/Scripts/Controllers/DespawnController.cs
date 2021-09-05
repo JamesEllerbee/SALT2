661640711eecb075b19144af3b6a8d2176da6d2b
Patrick Dooley <patrickthe8@live.com>
2021-09-05 17:25:04 -0400

    65 implement player crouching (#79)
    * Minor bug fixes.
    
    * Added ability for player to crouch, added despawn barrier to camera.

---
+++ SALT2/Scripts/Controllers/DespawnController.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public class DespawnController : Area
+{
+    /// <summary>
+    /// Hit detection code.
+    /// </summary>
+    /// <param name="body">Value for body that was hit by despaner.</param>
+    public void _on_Despawner_body_entered(PhysicsBody body)
+    {
+        if (body.IsInGroup("Enemy"))
+        {
+            // Despawn enemy.
+            body.QueueFree();
+        }
+    }
+}
