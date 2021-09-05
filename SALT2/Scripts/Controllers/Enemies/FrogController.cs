80c3fdd0cef2d838c5f0807434af1635beb221da
James Ellerbee <jamesellerbee1@gmail.com>
2021-09-04 20:37:26 -0500

    46 rusher ground frog controller (#54)
    * frog controller refactoring, player detection
    
    * remove ignored files
    
    * prefab updates

---
+++ SALT2/Scripts/Controllers/Enemies/FrogController.cs
@@ -0,0 +1,109 @@
+﻿using Godot;
+using SALT2.Scripts.Enumerations;
+using System;
+
+namespace SALT2.Scripts.Controllers.Enemies
+{
+    /// <summary>
+    /// Script that controls the Frog enemy types.
+    /// </summary>
+    public abstract class FrogController : KinematicBody
+    {
+
+        /// <summary>
+        /// The direction modifier determines which direction the entity should begin moving in.
+        /// </summary>
+        [Export]
+        private int directionModifier = 1;
+
+        [Export]
+        private long changeDirectionMs;
+
+        /// <summary>
+        /// Gets or sets this entity's enemy type.
+        /// </summary>
+        [Export]
+        public FrogType EnemyType { get; protected set; }
+
+        /// <summary>
+        /// Gets or sets the movement speed of this entity.
+        /// </summary>
+        [Export]
+        public float MoveSpeed { get; protected set; }
+
+        /// <summary>
+        /// Gets or sets the gravity of this entity.
+        /// </summary>
+        [Export]
+        public float Gravity { get; protected set; }
+
+        /// <summary>
+        /// Gets or sets the moving period is how long this entity will move in a given direction.
+        /// </summary>
+        [Export]
+        public long MovingPeriod { get; protected set; }
+
+        /// <summary>
+        /// Gets the value indicating the current direction this entity is facing.
+        /// </summary>
+        public int CurrentFacingDirection { get => directionModifier; }
+
+        /// <summary>
+        /// Gets or sets vertical velocity.
+        /// </summary>
+        public float VerticalVelocity { get; protected set; }
+
+
+        /// <inheritdoc/>
+        public override void _Ready()
+        {
+            base._Ready();
+
+            GD.Print($"Speed={MoveSpeed}, Period={MovingPeriod}");
+
+            // set the inital change direction period.
+            changeDirectionMs = GetNextChangeDirectionPeriod();
+        }
+
+        /// <inheritdoc/>
+        public override void _PhysicsProcess(float delta)
+        {
+            base._PhysicsProcess(delta);
+
+            // during the process method, apply movement along the "walk cycle"
+            var currentTime = GetCurrentTime();
+            if (IsTimeToChange(currentTime))
+            {
+                // update direction modifier
+                directionModifier *= -1;
+
+                // update next change directino interval
+                changeDirectionMs = GetNextChangeDirectionPeriod();
+            }
+
+            // apply movement
+            VerticalVelocity -= Gravity * delta;
+            MoveAndSlide(new Vector3(directionModifier * MoveSpeed, VerticalVelocity, 0), Vector3.Up);
+
+            if (IsOnFloor())
+            {
+                VerticalVelocity = -0.1F;
+            }
+        }
+
+        private long GetNextChangeDirectionPeriod()
+        {
+            return GetCurrentTime() + MovingPeriod;
+        }
+
+        private long GetCurrentTime()
+        {
+            return DateTimeOffset.Now.ToUnixTimeMilliseconds();
+        }
+
+        private bool IsTimeToChange(long currentTime)
+        {
+            return changeDirectionMs - currentTime <= 0L;
+        }
+    }
+}
