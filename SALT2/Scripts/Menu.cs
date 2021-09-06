15886e4dead83cf2243691c136cca96d576533f9
unknown <dmurtagh14@gmail.com>
2021-09-06 11:44:50 -0400

    added sprite animation to main menu

--- SALT2/Scripts/Menu.cs
+++ SALT2/Scripts/Menu.cs
@@ -6,24 +6,14 @@ using Godot;
 /// </summary>
 public class Menu : Control
 {
+	
 	/// <inheritdoc/>
 	public override void _Ready()
 	{
 		GetNode<TextureButton>("VBoxContainer/StartButton").GrabFocus();
+		GetNode<AnimatedSprite>("AnimatedSprite").Play("Idle", false);
 	}
 
-	// why is this code commented out?
-	/*
-	public override void _Input(InputEvent inputEvent)
-	{
-		if (inputEvent.IsActionPressed("move_down") && )
-		{
-			GetNode<Button>("VBoxContainer/OptionsButton").FocusMode;
-			GetFocusMode();
-
-		}
-	}
-	 */
 
 	/// <inheritdoc/>
 	public override void _Process(float delta)
@@ -35,25 +25,38 @@ public class Menu : Control
 		}
 	}
 
-	private void _on_StartButton_pressed()
+	private async void _on_StartButton_pressed()
 	{
+		GetNode<AnimatedSprite>("AnimatedSprite").Play("Shoot", false);
 		GetNode<AudioStreamPlayer>("StartSoundFX").Play();
+		
+		await ToSignal(GetTree().CreateTimer(0.5f, true), "timeout");
 
-		// todo: update this to the level scene.
 		GetTree().ChangeScene("res://Scenes/dev_testnew.tscn");
 	}
-
-	private void _on_QuitButton_pressed()
+	
+	private void _on_StartButton_mouse_exited()
 	{
+	GetNode<AnimatedSprite>("AnimatedSprite").Play("Walk", false);
+	}
+
+
+	private async void _on_QuitButton_pressed()
+	{
+		GetNode<AnimatedSprite>("AnimatedSprite").Play("Death", false);
+		await ToSignal(GetTree().CreateTimer(0.5f, true), "timeout");
 		GetTree().Quit();
 	}
 
-	private void _on_OptionsButton_pressed()
+	private async void _on_OptionsButton_pressed()
 	{
+		GetNode<AnimatedSprite>("AnimatedSprite").Play("Jump", false);
+		await ToSignal(GetTree().CreateTimer(0.5f, true), "timeout");
 		GetNode<VBoxContainer>("VBoxContainer").Visible = false;
 		GetNode<HSlider>("VolumeSlider").Visible = true;
 		GetNode<CheckButton>("FullScreenToggle").Visible = true;
 		GetNode<TextureButton>("MainMenuButton").Visible = true;
+		GetNode<TextureButton>("MainMenuButton").GrabFocus();
 	}
 
 	private void _on_VolumeSlider_value_changed(float value)
@@ -66,27 +69,36 @@ public class Menu : Control
 		OS.WindowFullscreen = !OS.WindowFullscreen; // toggles full screeen off/on
 	}
 
-	private void _on_MainMenuButton_pressed()
+	private async void _on_MainMenuButton_pressed()
 	{
+		GetNode<AnimatedSprite>("AnimatedSprite").Play("Jump", false);
+		await ToSignal(GetTree().CreateTimer(0.5f, true), "timeout");
+		GetNode<AnimatedSprite>("AnimatedSprite").Play("Walk", false);
 		GetNode<VBoxContainer>("VBoxContainer").Visible = true;
 		GetNode<HSlider>("VolumeSlider").Visible = false;
 		GetNode<CheckButton>("FullScreenToggle").Visible = false;
 		GetNode<TextureButton>("MainMenuButton").Visible = false;
+		GetNode<AnimatedSprite>("AnimatedSprite").Position = new Vector2(230f, 450.083f);
 	}
 
 	private void _on_StartButton_mouse_entered()
 	{
 		GetNode<TextureButton>("VBoxContainer/StartButton").GrabFocus();
+		GetNode<AnimatedSprite>("AnimatedSprite").Position = new Vector2(230f, 367.114f);
+		
 	}
 
 	private void _on_OptionsButton_mouse_entered()
 	{
 		GetNode<TextureButton>("VBoxContainer/OptionsButton").GrabFocus();
+		GetNode<AnimatedSprite>("AnimatedSprite").Position = new Vector2(230f, 450.083f);
+		
 	}
 
 	private void _on_QuitButton_mouse_entered()
 	{
 		GetNode<TextureButton>("VBoxContainer/QuitButton").GrabFocus();
+		GetNode<AnimatedSprite>("AnimatedSprite").Position = new Vector2(230f, 534.173f);
 	}
 
 	private void _on_StartButton_focus_entered()
@@ -103,4 +115,11 @@ public class Menu : Control
 	{
 		GetNode<AudioStreamPlayer>("SelectSoundFX").Play();
 	}
+	
+	private void _on_MainMenuButton_focus_entered()
+	{
+		GetNode<AnimatedSprite>("AnimatedSprite").Position = new Vector2(645f, 446f);
+		GetNode<AnimatedSprite>("AnimatedSprite").Play("Walk", false);
+	}
+	
 }
