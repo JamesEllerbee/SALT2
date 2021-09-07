6a6555df2d3161f1f83a30cd1b9eb44f32aa38ec
unknown <dmurtagh14@gmail.com>
2021-09-07 01:12:26 -0400

    added sound fx for cucumber

--- SALT2/Scripts/Controllers/CucumberController.cs
+++ SALT2/Scripts/Controllers/CucumberController.cs
@@ -6,67 +6,72 @@ using Godot;
 /// </summary>
 public class CucumberController : Area
 {
-    [Export]
-    private int restoreAmount = 1;
+	[Export]
+	private int restoreAmount = 1;
 
-    private KinematicBody player;
-    private Spatial graphics;
+	private KinematicBody player;
+	private Spatial graphics;
 
-    [Export]
-    private bool isActive = true;
+	[Export]
+	private bool isActive = true;
 
-    private bool wasUsed = false;
+	private bool wasUsed = false;
 
-    /// <summary>
-    /// Gets a value indicating whether if this entity is active or not.
-    /// </summary>
-    public bool IsActive { get => isActive; }
+	/// <summary>
+	/// Gets a value indicating whether if this entity is active or not.
+	/// </summary>
+	public bool IsActive { get => isActive; }
 
-    /// <inheritdoc/>
-    public override void _Ready()
-    {
-        player = (KinematicBody)GetNode("/root/Main/Player");
+	/// <inheritdoc/>
+	public override void _Ready()
+	{
+		player = (KinematicBody)GetNode("/root/Main/Player");
 
-        if (player == null)
-        {
-            GD.PrintErr("[Cucumber Controller] Could not resolve player");
-        }
+		if (player == null)
+		{
+			GD.PrintErr("[Cucumber Controller] Could not resolve player");
+		}
 
-        graphics = (Spatial)GetNode("Graphics");
+		graphics = (Spatial)GetNode("Graphics");
 
-        if (graphics == null)
-        {
-            GD.PrintErr("[Cucumber Controller] Could not resolve Graphics node.");
-        }
+		if (graphics == null)
+		{
+			GD.PrintErr("[Cucumber Controller] Could not resolve Graphics node.");
+		}
+		
+		//GetNode<AudioStreamPlayer>("CucumberSoundFX").Play();
 
-        GD.Print(this.Translation);
-    }
+		GD.Print(this.Translation);
+	}
 
-    /// <inheritdoc/>
-    public override void _Process(float delta)
-    {
-        base._Process(delta);
+	/// <inheritdoc/>
+	public override void _Process(float delta)
+	{
+		base._Process(delta);
 
-        if (wasUsed)
-        {
-            Free();
-        }
-    }
+		if (wasUsed)
+		{
+			System.Threading.Thread.Sleep(1);
+			Free();
+		}
+	}
 
-    /// <summary>
-    /// Hit detection.
-    /// </summary>
-    /// <param name="body">The body that entered this entity.</param>
-    public void OnCumcumberBodyEntered(PhysicsBody body)
-    {
-        if (body.IsInGroup("Player") && !wasUsed)
-        {
-            var playerController = body as PlayerController;
-            playerController.UpdateHitPoints(restoreAmount * -1);
+	/// <summary>
+	/// Hit detection.
+	/// </summary>
+	/// <param name="body">The body that entered this entity.</param>
+	public async void OnCumcumberBodyEntered(PhysicsBody body)
+	{
+		if (body.IsInGroup("Player") && !wasUsed)
+		{
+			GetNode<AudioStreamPlayer>("CucumberSoundFX").Play();
+			await ToSignal(GetTree().CreateTimer(0.3f, true), "timeout");
+			var playerController = body as PlayerController;
+			playerController.UpdateHitPoints(restoreAmount * -1);
 
-            wasUsed = true;
-            CollisionMask = 0b0;
-            graphics.Visible = false;
-        }
-    }
+			wasUsed = true;
+			CollisionMask = 0b0;
+			graphics.Visible = false;
+		}
+	}
 }
